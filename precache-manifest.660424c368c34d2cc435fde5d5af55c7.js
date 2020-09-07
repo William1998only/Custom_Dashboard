f16e5e4b1b4ea9db54fc9da6e7fc5b7cd2ba7752
William1998only <WilliamGolden54@gmail.com>
2020-09-07 14:04:31 +0700

    Updates

---
+++ precache-manifest.660424c368c34d2cc435fde5d5af55c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8af4d5c36c120b23d88956b74f5c597d",
+    "url": "/Custom_Dashboard/index.html"
+  },
+  {
+    "revision": "6b0184afe795c1b4859d",
+    "url": "/Custom_Dashboard/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "acd8abaafccaa84a76ba",
+    "url": "/Custom_Dashboard/static/js/2.ef26c21e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Custom_Dashboard/static/js/2.ef26c21e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b0184afe795c1b4859d",
+    "url": "/Custom_Dashboard/static/js/main.70520c7c.chunk.js"
+  },
+  {
+    "revision": "14ee2e4f663c9eab86b4",
+    "url": "/Custom_Dashboard/static/js/runtime-main.af41e5e9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Custom_Dashboard/static/media/logo.5d5d9eef.svg"
+  }
+]);
